fc2e58e1c5408ebabb1fe4059ef89fe3e9fb833a
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-05-21 09:18:07 -0300

    finalizando o estilo do menu aside

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,9 +1,33 @@
-import { Container } from "./styles";
+import { Container, Header, LogImg, MenuContainer, MenuItemLink, Title } from "./styles";
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from "react-icons/md";
+
+import logoImg from "../../assets/logo.svg";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogImg src={logoImg} alt="Logo minha carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saidas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,4 +4,48 @@ export const Container = styled.div`
   grid-area: AS;
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.secondary};
+
+  padding-left: 20px;
+
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`;
+
+export const LogImg = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+
+export const Title = styled.h3`
+  color: ${(props) => props.theme.colors.white};
+  margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MenuItemLink = styled.a`
+  color: ${(props) => props.theme.colors.info};
+  text-decoration: none;
+
+  margin: 0 7px;
+  transition: opacity 0.3s;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  &:hover {
+    opacity: 0.7;
+  }
+  > svg {
+    font-size: 18px;
+  }
 `;
