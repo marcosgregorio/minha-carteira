094b6e01275d3b71f81f7679ac05e893876407cd
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-05-22 08:07:08 -0300

    estilzando a scrollbar firefox e chrome

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -5,4 +5,30 @@ export const Container = styled.div`
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.primary};
   padding: 25px;
+
+  /* pego o total do view menos o tamanho 
+    do header que e de 70px.
+    Esse calculo resulta no tamanho total q deve aparecer em tela */
+  height: calc(100vh - 70px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius: 10px;
+    width: 5px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.tertiary};
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #30336f;
+  }
+
+  scrollbar-color: ${(props) => props.theme.colors.secondary} // @ts-expect-error
+    ${(props) => props.theme.colors.tertiary};
 `;
--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Container, Content } from "./styles";
+import { Container } from "./styles";
 import { ContentHeader } from "../../components/ContentHeader/ContentHeader";
 import { SelectInput } from "../../components/SelectInput/SelectInput";
 import { useTheme } from "styled-components";
 import { HistoryFinanceCard } from "../../components/HistoryFinanceCard/HistoryFinanceCard";
+import Content from "../../components/Content/Content";
 const List: React.FC = () => {
   const theme = useTheme();
   const options = [
@@ -22,6 +23,55 @@ const List: React.FC = () => {
         <SelectInput options={options} />
       </ContentHeader>
       <Content>
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
+        <HistoryFinanceCard
+          cardColor={theme.colors.tertiary}
+          tagColor={theme.colors.warning}
+          title="Conta de luz"
+          subtitle="21/05/2024"
+          amount="399"
+        />
         <HistoryFinanceCard
           cardColor={theme.colors.tertiary}
           tagColor={theme.colors.warning}
