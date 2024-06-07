b2efa0142ad8fbf26a5880fe578101fdfdfeeda9
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-06-07 07:46:03 -0300

    feat: criando os inputs no dashboard

--- src/pages/Dashbord/Dashbord.tsx
+++ src/pages/Dashbord/Dashbord.tsx
@@ -1,24 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./styles";
 import { ContentHeader } from "../../components/ContentHeader/ContentHeader";
 import { SelectInput } from "../../components/SelectInput/SelectInput";
+import { useTheme } from "styled-components";
 
 const Dashboard: React.FC = () => {
-  const options = [
+  const [monthSelected, setMonthSelected] = useState<string | number | undefined>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<string | number | undefined>(
+    String(new Date().getFullYear())
+  );
+  const months = [
     {
-      value: "Marcola",
-      label: "marcola",
+      value: 1,
+      label: "Janeiro",
     },
     {
-      value: "Maria",
-      label: "maria",
+      value: 2,
+      label: "Fevereiro",
+    },
+    {
+      value: 3,
+      label: "Marco",
+    },
+    {
+      value: 4,
+      label: "Abril",
+    },
+    {
+      value: 5,
+      label: "Maio",
+    },
+    {
+      value: 6,
+      label: "Junho",
+    },
+    {
+      value: 7,
+      label: "Julho",
+    },
+    {
+      value: 8,
+      label: "Agosto",
+    },
+    {
+      value: 9,
+      label: "Setembro",
     },
   ];
-
+  const years = [
+    {
+      value: 2024,
+      label: 2024,
+    },
+    {
+      value: 2023,
+      label: 2023,
+    },
+    {
+      value: 2022,
+      label: 2022,
+    },
+    {
+      value: 2020,
+      label: 2020,
+    },
+  ];
   return (
     <Container>
-      <ContentHeader title="Dashboard" linecolor="#000">
-        <div></div>
+      <ContentHeader title="Dashboard" linecolor={`${useTheme().colors.info}`}>
+        <SelectInput
+          defaultValue={monthSelected}
+          onChange={(value) => setMonthSelected(value)}
+          options={months}
+        />
+        <SelectInput
+          defaultValue={yearSelected}
+          onChange={(newValue) => setYearSelected(newValue)}
+          options={years}
+        />
         {/* <SelectInput options={options} /> */}
       </ContentHeader>
     </Container>
