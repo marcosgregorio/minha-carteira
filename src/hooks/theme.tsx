0774ed1ed16adf517c921ae549e9fb1df2d6ca39
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-07-25 16:47:07 -0300

    feat: Update theme toggle functionality and styles

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -3,53 +3,61 @@ import dark from "../styles/themes/dark";
 import light from "../styles/themes/light";
 
 interface ThemeContextData {
-    toggleTheme(): void;
-    theme: ITheme;
+  toggleTheme(): void;
+  theme: ITheme;
 }
 
 interface ITheme {
-    title: string;
-    colors: {
-        primary: string;
-        secondary: string;
-        tertiary: string;
+  title: string;
+  colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
 
-        white: string;
-        black: string;
-        gray: string;
+    white: string;
+    black: string;
+    gray: string;
 
-        success: string;
-        info: string;
-        warning: string;
-    };
+    success: string;
+    info: string;
+    warning: string;
+  };
 }
 
 const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData);
 
 type ThemeContextProviderProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-export const ThemeContextProvider: React.FC<ThemeContextProviderProps> = ({ children }) => {
-    const [theme, setTheme] = useState<ITheme>(dark);
+export const ThemeContextProvider: React.FC<ThemeContextProviderProps> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<ITheme>(() =>
+    localStorage.getItem("@minha-carteira:theme")
+      ? JSON.parse(localStorage.getItem("@minha-carteira:theme") as string)
+      : dark
+  );
 
-    const toggleTheme = () => {
-        if (theme.title === "dark") {
-            setTheme(light);
-        } else {
-            setTheme(dark);
-        }
+  const toggleTheme = () => {
+    if (theme.title === "dark") {
+      setTheme(light);
+      localStorage.setItem("@minha-carteira:theme", JSON.stringify(light));
+    } else {
+      setTheme(dark);
+      localStorage.setItem("@minha-carteira:theme", JSON.stringify(dark));
     }
+  };
 
-    return (
-        <ThemeContext.Provider value={{ toggleTheme, theme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-}
+  return (
+    <ThemeContext.Provider value={{ toggleTheme, theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 export function useTheme(): ThemeContextData {
-    const context = useContext(ThemeContext);
+  const context = useContext(ThemeContext);
 
-    return context;
+  return context;
 }
